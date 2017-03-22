364b8250fd3cacda5a27f38c70b4ac9ddceec988
MyatMyo <myat.myo.lwin@frobom.com>
2017-03-22 15:48:09 +0630

    modal window for brief_history and summary of scorm

---
+++ scorm/scorm-tool/src/java/org/sakaiproject/scorm/ui/console/pages/res/modalWindow.js
@@ -0,0 +1,8 @@
+function setModalData(title, data) {
+	document.getElementById("modal-title").innerHTML = title;
+	document.getElementById("modal-body-data").innerHTML = data;
+}
+/*function fixedBodyPadding() {
+	document.getElementsByTagName("body").style.paddingRight = "0px";
+	alert(document.getElementsByTagName("body").style.paddingRight+" aa ");
+}*/
