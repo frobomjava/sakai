cdbc39510f32a311d8a6753ecd225ee0a3e7bd66
aungmyohtet <aungmyohtet@outlook.com>
2017-11-23 09:49:05 +0630

    padding of left nav (scorm)

--- scorm/scorm-tool/src/webapp/scripts/resize.js
+++ scorm/scorm-tool/src/webapp/scripts/resize.js
@@ -34,10 +34,12 @@ function onResize() {
 		var navWidth = document.getElementById("scormNavPanel").offsetWidth;
 		var mainWidth = windowWidth - navWidth - 130;
 	
-		document.getElementById("scormNavPanel").style.height = mainHeight+"px";
-		document.getElementById("scormContentPanel").style.height = mainHeight+"px";
+		document.getElementById( "scormNavPanel" ).style.height = (mainHeight - 10) + "px";
+		document.getElementById( "scormContentPanel" ).style.height = (mainHeight - 10) + "px";
 		document.getElementById("scormContent").style.height = mainHeight+"px";
 		document.getElementById("scormContent").style.width = mainWidth + "px";
+
+		document.getElementById( "scormNavPanel" ).style.paddingBottom = "20px";
 	}
 }
 
